8aa8576c5f000ca28add927ad0d2b61cd918559f
haydarkadioglu <95019347+haydarkadioglu@users.noreply.github.com>
2026-07-20 14:06:27 +0300

    chore: ignore koza-installer directory

--- koza-installer/install.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-REM Koza Kurulum Baslatici
-REM Bu dosya kullanici cift tikladiginda PowerShell setup betigini calistirir.
-
-echo Koza Setup baslatiliyor, lutfen bekleyin...
-powershell.exe -NoProfile -ExecutionPolicy Bypass -WindowStyle Hidden -File "%~dp0setup.ps1"
-exit
